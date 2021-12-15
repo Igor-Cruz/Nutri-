1afbf3147b4fcbda416036c369d5a70847e11fdb
IGOR CRUZ <igorcruz.it@outlook.com>
2021-12-14 21:13:18 -0300

    Footer p-absolute

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -3,8 +3,8 @@ import React from "react"
 
 const Footer = () => {
   return (
-    <div className="bg-green-400 p-4">
-      <div className="container mx-auto text-center font-bold text-white">
+    <div className="bg-green-400 p-4 absolute inset-x-0 bottom-0">
+      <div className="container mx-auto text-center font-bold text-white ">
         Projeto desenvolvido por: Igor Cruz | {' '}
         <a className="hover:underline hover:text-black" href="https://www.linkedin.com/in/igor-cruz-ti/">Linkedin </a>| {' '}
         <a className="hover:underline hover:text-black" href="https://github.com/Igor-Cruz"> Github</a>
--- pages/index.js
+++ pages/index.js
@@ -6,10 +6,9 @@ const Index = () => {
     <div>
       <div>
         <img src='./2.jpg' className="bg-fixed" />
+
       </div>
-      <div className='m-4 text-center'>
-        <h1>Tenha uma vida + saudável</h1>
-      </div>
+
     </div>
   )
 }
